c19dd4b1b1401097b1911f76cdf750d77e14606a
hyugoxd <123418433+hyugoxd@users.noreply.github.com>
2023-01-31 09:39:09 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+"use strict";
+//líneas para probar
+
+let user='Juan', age='28', message='butifarras'
+const bobo='¿Qué miras?' //también sirve para colores en hexadecimal
+
+//alert('Me cago');
+//alert('Digref siempre gana');
+alert(`Hola, ${user}`); //hay que usar backticks `${}`
+
