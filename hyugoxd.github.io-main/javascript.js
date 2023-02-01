ad0c41d44f2b2b1c87c8778076fb6fced63d3c57
hyugoxd <123418433+hyugoxd@users.noreply.github.com>
2023-02-01 11:22:50 +0100

    Add files via upload

---
+++ hyugoxd.github.io-main/javascript.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let user='Juan', age='28', message='butifarras';
+const bobo='¿Qué miras?'; /*también sirve para colores en hexadecimal. prompt permite añadir valores, también hay confrim
+
+alert('Me cago');
+alert('Digref siempre gana');
+
+window.open('https://ign.com');
+button.onclick = () => {
+    window.open('https://ign.com');
+};
+
+alert(`Hola, ${user}`); //hay que usar backticks `${}` */
