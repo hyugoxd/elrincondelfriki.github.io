c77f11b2b97bab0a6132d830e6e05ecd50e9a703
hyugoxd <123418433+hyugoxd@users.noreply.github.com>
2023-02-01 17:43:21 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
 "use strict";
-//líneas para probar
 
-let user='Juan', age='28', message='butifarras'
-const bobo='¿Qué miras?' //también sirve para colores en hexadecimal
+let user='Juan', age='28', message='butifarras';
+const bobo='¿Qué miras?'; /*también sirve para colores en hexadecimal. prompt permite añadir valores, también hay confrim
 
-//alert('Me cago');
-//alert('Digref siempre gana');
-alert(`Hola, ${user}`); //hay que usar backticks `${}`
+alert('Me cago');
+alert('Digref siempre gana');
 
+window.open('https://ign.com');
+button.onclick = () => {
+    window.open('https://ign.com');
+};
+
+alert(`Hola, ${user}`); //hay que usar backticks `${}` */
